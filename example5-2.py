1733683041044935eba7abc2ec8f529628ccec37
Juanan Pereira <juanan.pereira@ehu.eus>
2016-12-16 00:28:04 +0100

    Fix example5-2.py

--- example5-2.py
+++ example5-2.py
@@ -1,11 +1,9 @@
 import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-credentials = ServiceAccountCredentials.from_json_keyfile_name('/Users/admin/Desktop/JuananAPI-565802092fce.json', scope)
 scope = ['https://spreadsheets.google.com/feeds']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('/Users/admin/Desktop/JuananAPI-565802092fce.json', scope)
 gc = gspread.authorize(credentials)
-ss = gc.open('Microbios')
 ss = gc.open('Microbios')
 ss.worksheets()
 ws = ss.worksheet('bugs')
