3c6563f2c508208ff2f1294ee9571003ba8c69fb
Juanan Pereira <juanan.pereira@ehu.eus>
2016-12-25 19:45:15 +0100

    Fix int parsing error

--- getting_soup.py
+++ getting_soup.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 BASE_URL = 'http://en.wikipedia.org'
 # Wikipedia will reject our request unless we add
@@ -15,9 +16,42 @@ def get_Nobel_soup():
     # Return the content of the response parsed by BeautifulSoup
     return BeautifulSoup(response.content, "lxml")
 
+def get_column_titles(table):
+    """ Get the Nobel categories from the table header """
+    cols = []
+    for th in table.select_one('tr').select('th')[1:]:
+       link = th.select_one('a')
+    # Store the category name and any Wikipedia link it has
+       if link:
+           cols.append({'name':link.text, 'href':link.attrs['href']})
+       else:
+         cols.append({'name':th.text, 'href':None})
+    return cols
+
 
 soup = get_Nobel_soup()
-
 table = soup.select_one('table.sortable.wikitable')
-
 table.select('th')
+
+cols = get_column_titles(table);
+
+
+def get_Nobel_winners(table):
+     cols = get_column_titles(table)
+     winners = []
+     for row in table.select('tr')[1:-1]:
+         year = int(re.findall( r'[\d]+' ,row.select_one('td').text)[0]) # gets 1st <td>
+         for i, td in enumerate(row.select('td')[1:]):
+             for winner in td.select('a'):
+                 href = winner.attrs['href']
+                 if not href.startswith('#endnote'):
+                     winners.append({
+                        'year': year,
+                        'category': cols[i]['name'],
+                        'name':winner.text,
+                        'link':winner.attrs['href']
+                    })
+     return winners
+
+winners = get_Nobel_winners(table)
+print winners
