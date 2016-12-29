26e5b97ee3bb8765598d0aab5cdd854b5fa8ab13
Juanan Pereira <juanan.pereira@ehu.eus>
2016-12-29 19:29:47 +0100

    Add example 6.6
    For this to work you'll have to install this PR in Scrapy
    https://github.com/scrapy/scrapy/pull/2193

--- nobel_winners/nobel_winners/pipelines.py
+++ nobel_winners/nobel_winners/pipelines.py
@@ -5,7 +5,34 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import scrapy
+from scrapy.contrib.pipeline.images import ImagesPipeline
+from scrapy.exceptions import DropItem
 
 class NobelWinnersPipeline(object):
     def process_item(self, item, spider):
         return item
+
+
+class DropNonPersons(object):
+    """ Remove non-person winners """
+
+    def process_item(self, item, spider):
+        if not item['gender']:
+            raise DropItem("No gender for %s"%item['name'])
+        return item
+
+
+class NobelImagesPipeline(ImagesPipeline):
+
+    def get_media_requests(self, item, info):
+        for image_url in item['image_urls']:
+            yield scrapy.Request(image_url)
+
+    def item_completed(self, results, item, info):
+        image_paths = [x['path'] for ok, x in results if ok]
+        if image_paths:
+            item['bio_image'] = image_paths[0]
+
+        return item
+
