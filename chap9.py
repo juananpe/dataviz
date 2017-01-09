05cfd7233753d234797108bb6ba0b31386b7ee44
Juanan Pereira <juanan.pereira@ehu.eus>
2017-01-10 00:07:11 +0100

    Load json from file

--- chap9.py
+++ chap9.py
@@ -1,11 +1,11 @@
 from panda_utils import *
 import pandas as pd
 
-# df = pd.read_json(open('/opt/dataviz/nobel_winners/nobel_winners/spiders/nobel_winners.json'))
-
-df = mongo_to_dataframe('nobel_prize', 'winners')
-df.info()
-
+df = pd.read_json(open('/opt/dataviz/nobel_winners/nobel_winners/spiders/nobel_winners_full.json'))
 # dataframe_to_mongo(df, 'nobel_prize', 'winners')
 
+# df = mongo_to_dataframe('nobel_prize', 'winners')
 
+df.info()
+df.describe(include=['object'])
+df.head()
