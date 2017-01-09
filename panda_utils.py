b0f9689e94a7606580024c860f6567c1b21085b2
Juanan Pereira <juanan.pereira@ehu.eus>
2017-01-09 23:39:31 +0100

    Fixed camelcase names

--- panda_utils.py
+++ panda_utils.py
@@ -1,16 +1,19 @@
-def mongo_to_dataframe(db_name, collection, query={}, host='localhost', port=27017, username=none, password=none, no_id=true):
+from accessmongo import *
+import pandas as pd
+
+def mongo_to_dataframe(db_name, collection, query={}, host='localhost', port=27017, username=None, password=None, no_id=True):
     """ create a dataframe from mongodb collection """
 
     db = get_mongo_database(db_name, host, port, username, password)
     cursor = db[collection].find(query)
-    df = pd.dataframe(list(cursor))
+    df = pd.DataFrame(list(cursor))
     if no_id:
         del df['_id']
 
     return df
 
 
-def dataframe_to_mongo(df, db_name, collection, host='localhost', port=27017, username=none, password=none):
+def dataframe_to_mongo(df, db_name, collection, host='localhost', port=27017, username=None, password=None):
     """ save a dataframe to mongodb collection """
     db = get_mongo_database(db_name, host, port, username, password)
     records = df.to_dict('records')
